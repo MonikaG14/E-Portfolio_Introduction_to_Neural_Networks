7a70094a9c68f87c800f5df3e9cc82317c5745eb
Monika <monikarg95@gmail.com>
2022-06-05 17:55:42 +0200

    initial commit

---
+++ code/Basics.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Here we will get to know and test the basics of NN such as
+# weights and dot product
+# the activation function and predictions
+# loss functions and the mean squared error
+# backpropagation with partial derivatives
+# and the chain rule
